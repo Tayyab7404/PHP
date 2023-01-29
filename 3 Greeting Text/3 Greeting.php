21f6a8b19c775c6656ddb20b9837919c7b4f6ac9
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-01-29 23:39:24 +0530

    Create 3 Greeting.php

---
+++ 3 Greeting Text/3 Greeting.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<title>Greeting Message</title>
+		<link rel="stylesheet" href="Greetingtyle.css">
+	</head>
+
+	<body>
+		<center><br><br>
+			<div class="double"><b>GREETING TEXT</b></div><br>
+
+			<div class="dashed">
+				<form action="" method="post">
+				<p>Enter your name:<br>
+				<input type="text" name="name"></p>
+				<input type="submit" value="&nbsp;&nbsp; SUBMIT &nbsp;&nbsp;">
+				</form>
+			</div>
+		</center>
+
+		<?php
+			if($_POST)
+			{
+				$name = $_POST["name"];
+
+				echo'<center><br>
+					 <font color="darkblue" face="arial" size="6">WELCOME</font></center>
+					 
+					 <center><br>
+					 <font color="red" face="calibri" size="6"><name><b>' . $name . '</b></name>
+					 </font></center>
+					 
+					 <p><img src="flower1.png" alt="flower" style="float:right;">
+					 <img src="flower2.png" alt="flower" style="float:left;"></p>';
+			}
+		?>
+	</body>
+</html>
