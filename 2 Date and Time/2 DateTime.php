2a4624900fcdd69bf8e3a1de6cd943b7fe16f8a3
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-01-29 23:38:32 +0530

    Create 2 DateTime.php

---
+++ 2 Date and Time/2 DateTime.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<title>Date and Time</title>
+	</head>
+	
+	<body bgcolor="lightgreen">
+		<style>
+			table
+			{
+				font-family:calibri;
+			}
+		</style>
+		
+		<?php
+			date_default_timezone_set("Asia/Kolkata");
+			echo'<table bgcolor="skyblue" align="center" border="2" width="50%" height="50%">
+				 <th>Format</th>
+				 <th>Date and Time</th>
+				 
+				 <tr align="center"><td>Date in dd/mm/yy format: </td>
+				 <td>'.date("d/m/y").'</td></tr>
+				 
+				 <tr align="center"><td>Current date in dd/mm/yyyy format: </td>
+				 <td>'.date("d/m/Y").'</td></tr>
+				 
+				 <tr align="center"><td>Current date in dd MON yyyy format: </td>
+				 <td>'.date("d M Y").'</td></tr>
+				 
+				 <tr align="center"><td>Current date in Day, dd MON yyyy format: </td>
+				 <td>'.date("D, d M Y").'</td></tr>
+
+				 <tr align="center"><td>Current date in Day, dd MONTH yyyy format: </td>
+				 <td>'.date("D, d F Y").'</td></tr>
+			
+				 <tr align="center"><td>Time in 12-Hour Format:</td>
+				 <td>'.date("h : i : s a").'</br></td></tr>
+				 <tr align="center"><td>Time in 24-Hour Format:</td>
+				 <td>'.date("H : i : s ").'</br></td></tr>
+				 </table>';
+		?>
+	</body>
+</html>
