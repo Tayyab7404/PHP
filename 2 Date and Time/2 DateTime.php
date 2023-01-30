6faa28e53d5195a85dcce381c22cdc66314df325
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-01-30 20:23:41 +0530

    Add files via upload

--- 2 Date and Time/2 DateTime.php
+++ 2 Date and Time/2 DateTime.php
@@ -18,26 +18,40 @@
 				 <th>Format</th>
 				 <th>Date and Time</th>
 				 
-				 <tr align="center"><td>Current Date in dd/mm/yy format: </td>
-				 <td>'.date("d/m/y").'</td></tr>
+				 <tr align="center">
+				 <td>Current Date in dd/mm/yy format: </td>
+				 <td>'.date("d/m/y").'</td>
+				 </tr>
 				 
-				 <tr align="center"><td>Current date in dd/mm/yyyy format: </td>
-				 <td>'.date("d/m/Y").'</td></tr>
+				 <tr align="center">
+				 <td>Current Date in dd/mm/yyyy format: </td>
+				 <td>'.date("d/m/Y").'</td>
+				 </tr>
 				 
-				 <tr align="center"><td>Current date in dd MON yyyy format: </td>
-				 <td>'.date("d M Y").'</td></tr>
+				 <tr align="center">
+				 <td>Current Date in dd MON yyyy format: </td>
+				 <td>'.date("d M Y").'</td>
+				 </tr>
 				 
-				 <tr align="center"><td>Current date in Day, dd MON yyyy format: </td>
-				 <td>'.date("D, d M Y").'</td></tr>
+				 <tr align="center">
+				 <td>Current Date in Day, dd MON yyyy format: </td>
+				 <td>'.date("D, d M Y").'</td>
+				 </tr>
 
-				 <tr align="center"><td>Current date in Day, dd MONTH yyyy format: </td>
-				 <td>'.date("D, d F Y").'</td></tr>
+				 <tr align="center">
+				 <td>Current Date in Day, dd MONTH yyyy format: </td>
+				 <td>'.date("D, d F Y").'</td>
+				 </tr>
 			
-				 <tr align="center"><td>Time in 12-Hour Format:</td>
-				 <td>'.date("h : i : s a").'</br></td></tr>
+				 <tr align="center">
+				 <td>Current Time in 12-Hour Format:</td>
+				 <td>'.date("h : i : s a").'</td>
+				 </tr>
 				 
-				 <tr align="center"><td>Time in 24-Hour Format:</td>
-				 <td>'.date("H : i : s ").'</br></td></tr>
+				 <tr align="center">
+				 <td>Current Time in 24-Hour Format:</td>
+				 <td>'.date("H : i : s ").'</td>
+				 </tr>
 				 </table>';
 		?>
 	</body>
