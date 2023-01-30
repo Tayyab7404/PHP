d1551f4b6badf4b66ef239bd3d6a44ee766dd4d2
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-01-30 20:05:28 +0530

    Add files via upload

--- 2 Date and Time/2 DateTime.php
+++ 2 Date and Time/2 DateTime.php
@@ -13,11 +13,12 @@
 		
 		<?php
 			date_default_timezone_set("Asia/Kolkata");
+			
 			echo'<table bgcolor="skyblue" align="center" border="2" width="50%" height="50%">
 				 <th>Format</th>
 				 <th>Date and Time</th>
 				 
-				 <tr align="center"><td>Date in dd/mm/yy format: </td>
+				 <tr align="center"><td>Current Date in dd/mm/yy format: </td>
 				 <td>'.date("d/m/y").'</td></tr>
 				 
 				 <tr align="center"><td>Current date in dd/mm/yyyy format: </td>
