17b801e1e84a14b7139dafca841c0fc7e04bb14a
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2023-02-01 23:23:36 +0530

    Update 6 Car Advertisement.php

--- 09 Car Advertisement/6 Car Advertisement.php
+++ 09 Car Advertisement/6 Car Advertisement.php
@@ -1,45 +1,86 @@
 <!DOCTYPE HTML>
 
 <html>
+	<head>
+		<title>Lamborghini</title>
+	</head>
+	
 	<body>
 		<style>
 		body
 		{
 			background-color:azure;
-    			background-image: url("inti1.jpg");
-    			background-repeat: no-repeat;
-    			background-size: cover;
+			background-image: url("inti1.jpg");
+			background-repeat: no-repeat;
+			background-size: cover;
 		}
 		</style>
 
+		<center>
 		<embed src = "Clarx H.A.Y.mp3"></embed>
+		</center>
+		<hr>
 
-		<p><b><center><font size=6 color='red'><u>LAMBORGHINI</u></b></font></center></p>
+		<center><p><b><font size=6 color='orange'>
+		<u>LAMBORGHINI
+		</u></font></b></p></center>
 
-		<table align=center cellpadding=5 cellspacing=9><th>
+		<table align=center cellpadding=5 cellspacing=9>
+		<th><p><font size=6 color=azure>
+		<b>LAMBORGHINI</b> is Refinement, Luxury and Perfection
+		</font></p></th>
 
-		<p><font size=6 color=azure><b>LAMBORGHINI</b> is Refinement, Luxury and Perfection</font></p> </th>
-
-		<tr><td><font size=4 color=azure>The city or the wild, the smooth tracks or the dusty trails,<br> S-Cross can take you wherever your destination lies. <br>
-		It's made to match your crossover life,<br> and give you a drive truly above the ordinary.
+		<tr><td><font size=4 color=azure>
+		The city or the wild, the smooth tracks or the dusty trails,
+		<br> 
+		S-Cross can take you wherever your destination lies. 
+		<br>
+		It's made to match your crossover life,
+		<br> 
+		and give you a drive truly above the ordinary.
 		</td></tr></table>
 
-		<center><img src='Lambo2.jpg' width='80%' height='600'></center><hr>
-
-		<p><font color='white' size=5><b>ABOVE THE ORDINARY FEATURES</b></p>
+		<hr>
+		<center><img src='Lambo2.jpg' width='80%' height='600'></center>
+		<p><font color='lightgreen' size=5>
+		<b><u>
+		ABOVE THE ORDINARY FEATURES
+		</u></b>
+		</p>
 
 		<table><tr>
-
-		<td><p><font color='white' size=5>SmartPlay Infotainment<br>System 18 cm(7)</p><img src='inti10.jpg' width='75%' height='75%'></td>
-
-		<td><p><font color='white' size=5>Excellent <br>Cruise Control</p><img src='inti11.jpg' width='100%' height='100%'></td>
+		<td><p><font color='lightgreen' size=5>
+		<b>SmartPlay Infotainment<br>System 18 cm(7)
+		</b></p>
+		<img src='inti10.jpg' width='75%' height='75%'>
+		</td>
+		
+		<td><p><font color='lightgreen' size=5>
+		<b>Excellent<br>Cruise Control
+		</b></p>
+		<img src='inti11.jpg' width='100%' height='100%'>
+		</td>
  
-		<td><p><font color='white' size=5>Steering Mounted Audio & <br>Voice controls</p><img src='inti12.jpg' width='75%' height='75%'></td>
+		<td><p><font color='lightgreen' size=5>
+		<b>Steering Mounted Audio & <br>Voice controls
+		</b></p>
+		<img src='inti12.jpg' width='75%' height='75%'>
+		</td>
 	
-		<td><p><font color='white' size=5>Auto Rain sensing<br> wipers</p><img src='inti13.jpg' width='75%' height='75%'></td>
-
-		<p align='center'><font color='AZURE' size=5>HYDERABAD</p></font>
-
-		<h2 align='center'><font color='orange'> <b>TAYYAB AUTOMOBILES: 90323 01700</b></h2><hr>
+		<td><p><font color='lightgreen' size=5>
+		<b>Auto Rain sensing<br> wipers
+		</b></p>
+		<img src='inti13.jpg' width='75%' height='75%'>
+		</td></tr></table>
+		
+		<hr>
+		<table><tr>
+		<p align='center'><font color='AZURE' size=5>
+		<b>HYDERABAD
+		</b></p></font>
+		<h2 align='center'><font color='orange' size=5> 
+		<b>TAYYAB AUTOMOBILES: 9032301700
+		</b></h2></tr></table>
+		<hr>
 	</body>
 </html>
